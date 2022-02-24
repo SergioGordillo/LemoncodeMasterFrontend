78b658fb9f891afd0ea085de9fcccd1effd7e137
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-02-24 13:18:39 +0100

    Exercise done

--- 02-JavaScript/01-JS-Fundamentals/BiggestWord/main.js
+++ 02-JavaScript/01-JS-Fundamentals/BiggestWord/main.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+
+var phrase = "This is wonderful abcdefghi";
+
+function biggestWord(phrase) {
+
+    words = phrase.split(" ");
+    wordsLength = new Array;
+
+    thisIsTheBiggestWord = new Array;
+    thisIsTheBiggestWord.push(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        const element = words[i].length;
+        wordsLength.push(element);
+
+        if (element > thisIsTheBiggestWord[0].length) {
+            thisIsTheBiggestWord.shift();
+            thisIsTheBiggestWord.push(words[i]);
+        }
+    }
+    console.log("Phrase: ", phrase);
+    console.log("This is the biggest word: ", thisIsTheBiggestWord);
+
+}
+
+biggestWord(phrase);
+
+
