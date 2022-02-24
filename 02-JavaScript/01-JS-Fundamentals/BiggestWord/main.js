0b0701673b4c4c94630a182db9561c9ff8da03c5
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-02-24 13:22:46 +0100

    feat: code optimized and cleaner

--- 02-JavaScript/01-JS-Fundamentals/BiggestWord/main.js
+++ 02-JavaScript/01-JS-Fundamentals/BiggestWord/main.js
@@ -4,16 +4,13 @@ var phrase = "This is wonderful abcdefghi";
 function biggestWord(phrase) {
 
     words = phrase.split(" ");
-    wordsLength = new Array;
 
     thisIsTheBiggestWord = new Array;
     thisIsTheBiggestWord.push(" ");
 
     for (let i = 0; i < words.length; i++) {
-        const element = words[i].length;
-        wordsLength.push(element);
 
-        if (element > thisIsTheBiggestWord[0].length) {
+        if (words[i].length > thisIsTheBiggestWord[0].length) {
             thisIsTheBiggestWord.shift();
             thisIsTheBiggestWord.push(words[i]);
         }
