c07981853bad0d5034b98af010bcc9b69511adcf
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-02-27 19:57:48 +0100

    feat: just need to solve the last switch

--- 02-JavaScript/01-JS-Fundamentals/Califications/main.js
+++ 02-JavaScript/01-JS-Fundamentals/Califications/main.js
@@ -1,3 +1,5 @@
+// DATA
+
 const eso2 = {
     David: 8.25,
     Maria: 9.5,
@@ -7,25 +9,64 @@ const eso2 = {
     Carmen: 8,
 };
 
-function showValues(eso2) {
-    console.log(Object.values(eso2));
+// HELPERS
+
+function getLengthOfObject(eso2) { //Function that returns the length of a given object
+    let lengthOfObject = Object.keys(eso2).length;
+    return lengthOfObject; // 6 
 }
 
-function calculateAverageMark(eso2) {
-    const studentsNumber = eso2.length;
-    const marks = Object.values(eso2);
-    const totalMarks = 0;
+function calculateTotalStudentMarks(eso2) { //Function that returns the total of the marks of the students
+    const totalStudentMarks = Object.values(eso2).reduce((total, actual) => {
+        return total + actual;
+    }, 0);
+    return totalStudentMarks;
+}
 
-    for (let i = 0; i < eso2.length; i++) {
-        totalMarks += eso2[i];
-        console.log(totalMarks);
+function calculateAverageMark(totalStudentMarks, studentsNumber) { //Function that calculates the average Mark of all the class
+    const averageMark = totalStudentMarks / studentsNumber;
+    return averageMark;
+}
+
+function calculateSpanishClassAverageMark(averageMark) {
+    console.log("me muestra mi nota media", averageMark);
+    var markText = " ";
+
+    switch (averageMark) {
+        case 10:
+            markText = "Esta clase ha obtenido una matrícula de honor"
+            break;
+        case averageMark >= 9 && averageMark < 10:
+            markText = "Esta clase ha obtenido un sobresaliente"
+            break;
+        case averageMark >= 7 && averageMark < 9:
+            markText = "Esta clase ha obtenido un notable"
+            break;
+        case averageMark >= 6 && averageMark < 7:
+            markText = "Esta clase ha obtenido un bien"
+            break;
+        case averageMark >= 5 && averageMark < 6:
+            markText = "Esta clase ha obtenido un suficiente"
+            break;
+        case averageMark >= 4 && averageMark < 5:
+            markText = "Esta clase ha obtenido un insuficiente"
+            break;
+        case averageMark < 4:
+            markText = "Esta clase ha obtenido un muy deficiente"
+            break;
+        default:
+            break;
     }
 
-    console.log(totalMarks);
-
-
-
+    return console.log("Marktext", markText);
 }
 
-showValues(eso2);
-calculateAverageMark(eso2);
+// MAIN CODE OF THE PROGRAM
+
+const studentsNumber = getLengthOfObject(eso2);
+console.log("studentsNumber", studentsNumber); //OK
+const totalStudentMarks = calculateTotalStudentMarks(eso2);
+console.log("totalStudentMarks", totalStudentMarks); //OK
+const averageMark = calculateAverageMark(totalStudentMarks, studentsNumber);
+console.log("averageMark", averageMark);
+calculateSpanishClassAverageMark(averageMark);
