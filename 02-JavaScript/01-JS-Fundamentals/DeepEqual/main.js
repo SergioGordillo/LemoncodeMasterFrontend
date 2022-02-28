426fdf30c9552f069d70add16e3ad4705f62cb72
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-02-28 12:30:10 +0100

    first exercise done

--- 02-JavaScript/01-JS-Fundamentals/DeepEqual/main.js
+++ 02-JavaScript/01-JS-Fundamentals/DeepEqual/main.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+// EXERCISE A
+
+var user = { name: "María", age: 30 };
+var clonedUser = { name: "María", age: 30 };
+console.log(user === clonedUser); // false
+
+function isEqual(user, clonedUser) {
+    var samePropertiesOrNot = Object.keys(user).every(p => clonedUser.hasOwnProperty(p));
+    return samePropertiesOrNot;
+}
+
+console.log(isEqual(user, clonedUser)); // true
+
+// EXERCISE B
+
+var friend = {
+    name: "María",
+    age: 30,
+    address: { city: "Málaga", code: 29620 },
+    friends: ["Juan"],
+};
+var clonedFriend = {
+    name: "María",
+    age: 30,
+    address: { city: "Málaga", code: 29620 },
+    friends: ["Juan"],
+};
+
+function isDeepEqual(a, b) {
+
+}
+console.log(isDeepEqual(user, clonedUser)); // true
