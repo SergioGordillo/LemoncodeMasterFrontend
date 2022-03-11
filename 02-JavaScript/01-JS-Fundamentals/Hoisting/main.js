9e7c0740937d7def87d2c41f773e0ae38940f295
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-03-11 23:43:44 +0100

    exercise done

--- 02-JavaScript/01-JS-Fundamentals/Hoisting/main.js
+++ 02-JavaScript/01-JS-Fundamentals/Hoisting/main.js
@@ -1,1 +1,37 @@
-console.log("Hello world");
+// FIRST EXERCISE
+
+// function f() {
+//     console.log(a); // undefined
+//     console.log(g()); //undefined
+//     var a = "good job!";
+//     function g() {
+//         return a;
+//     }
+//     console.log(a); //goodjob
+// }
+// f();
+
+// SECOND EXERCISE
+
+// var a = 1;
+// (function () {
+//     console.log(a); //undefined
+//     var a = 2;
+//     b = 4;
+//     var c = 3;
+// })();
+// console.log(a); //1
+// console.log(b); //4
+// console.log(c); //error is not defined
+
+// THIRD EXERCISE
+f();
+var a = 1;
+function f() {
+    console.log(a); //undefined
+    b = 4;
+    var c = 3;
+}
+console.log(a); //1
+console.log(b); //4
+console.log(c); //error is not defined
