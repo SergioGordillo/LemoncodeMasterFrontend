98dd33cf01887c6cafc911d91eb3e4f3b3707efa
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-03-12 21:01:37 +0100

    exercise done

--- 02-JavaScript/01-JS-Fundamentals/Primes/main.js
+++ 02-JavaScript/01-JS-Fundamentals/Primes/main.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+
+// AUX FUNCTION
+function isPrime(num) { //It gives you the information about a number being a prime one or not
+    if (num <= 1) return false;
+
+    for (var i = 2, prime = true; i < num && prime; i++) {
+        prime = num % i !== 0;
+    }
+    return prime;
+}
+
+// MAIN FUNCTION
+function showPrimeNumbers(from, to) { //It loops over the interval of numbers
+    for (var i = from; i <= to; i++) {
+        console.log(i.toString() + (isPrime(i) ? " is a prime number!" : " is not a prime number"));
+    }
+}
+
+showPrimeNumbers(1, 10);
+
