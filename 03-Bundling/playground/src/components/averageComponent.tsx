a2a4c71dd77f7bf06b9fc41c44763475ca042fec
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-04-18 23:18:54 +0200

    Third Bundling session

---
+++ 03-Bundling/playground/src/components/averageComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getAvg } from "../averageService";
+
+
+export const AverageComponent: React.FC = () => {
+    const [average, setAverage] = React.useState<number>(0);
+
+    React.useEffect(() => {
+        const scores = [90, 75, 60, 99, 94, 30];
+        setAverage(getAvg(scores));
+    }, []);
+
+    return (
+        <div>
+            <span>Students average: {average}</span>
+        </div>
+    );
+};
