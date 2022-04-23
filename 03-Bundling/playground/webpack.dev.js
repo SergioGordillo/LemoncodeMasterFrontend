b9d8cc1dfb4c9de6c850568ae93b6e864de04cea
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-04-23 12:07:24 +0200

    feat: configuration of webpack in dev and prod mode

---
+++ 03-Bundling/playground/webpack.dev.js
@@ -0,0 +1,31 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                exportLocalsConvention: "camelCase",
+                                localIdentName: "[path][name]__[local]--[hash:base64:5]"
+                            }
+                        },
+                    },
+                    "sass-loader"],
+            },
+        ]
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        port: "8081",
+        hot: true,
+    },
+    stats: "errors-only"
+});
---
+++ 03-Bundling/playground/webpack.prod.js
@@ -0,0 +1,31 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                exportLocalsConvention: "camelCase",
+                            }
+                        },
+                    },
+                    "sass-loader"],
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+    ],
+});
