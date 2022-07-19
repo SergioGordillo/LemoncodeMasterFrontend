cba418cf0427e18106ee5079a1b806744d5f0488
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-07-20 00:17:11 +0200

    restructuring of Layout1 to make it more simple to execute the exercises

--- 02-JavaScript/01-JS-Fundamentals/ZZCrypt/main.js
+++ 02-JavaScript/01-JS-Fundamentals/ZZCrypt/main.js
@@ -11,4 +11,37 @@ function decrypt(secret) {
     return secret.split("").map(decryptLetter).join("");
 }
 
-console.log(decrypt(secret));
+// var secret =
+//     "': rg!qg yq,urae: ghsrf wuran shrerg jq,u'qf ra r' ,qaq' er g'q,o rg,fuwurae: m!hfua( t'usqfuq ,:apu(:m xv";
+
+// // Sabiendo que el alfabeto original ha sufrido la siguiente transformación:
+// var plain = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+// var cipher = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+// function dictionary(plain, cipher) {
+//     let dictionary = {}
+
+//     for (let i = 0; i < plain.length; i++) {
+//         dictionary[cipher[i]] = plain[i];
+//     }
+
+//     dictionary[" "] = " ";
+//     return dictionary;
+// }
+
+
+
+// function decrypt(plain, cipher, secret) {
+
+//     let dic = dictionary(plain, cipher); // {"a" : "q"}
+
+//     let decypher = ""; // "sdasdadads"
+
+//     for (let i = 0; i < secret.length; i++) {
+//         decypher += dic[`${secret[i]}`];
+//     }
+
+//     return decypher;
+// }
+
+// console.log(decrypt(plain, cipher, secret));
