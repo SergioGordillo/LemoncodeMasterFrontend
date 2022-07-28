dc1ba9b5d9ebab1416f2f463f13ae60b70c79f61
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-07-28 23:30:41 +0200

    exercise done

--- 02-JavaScript/02-JS-ESNext/args/main.js
+++ 02-JavaScript/02-JS-ESNext/args/main.js
@@ -1,8 +1,13 @@
 function f(a, { b } = {}, c = 100) {
     console.log(arguments.length);
+    // console.log("a", a);
+    console.log("b", b)
+    console.log("args", arguments[1]);
     console.log(a, a === arguments[0]);
     console.log(b, b === arguments[1]);
     console.log(c, c === arguments[2]);
 }
 
-f("JS rocks!", { b: "b" }); 
+// f("JS rocks!", { b: "b" }); 
+f({ b: "b" });
+// f("JS sucks!", null, 13);
