c16d362103b5c9786721bc18bbbcadb622385f56
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-08-01 23:40:06 +0200

    i need to use spread and rest to be able to give a good solution to playersOrder

--- 02-JavaScript/02-JS-ESNext/playersOrder/main.js
+++ 02-JavaScript/02-JS-ESNext/playersOrder/main.js
@@ -1,5 +1,27 @@
-const beginningOrder = ["Ana", "Juan", "Pablo", "Lucia"]
+const beginningOrder = ["Ana", "Juan", "Pablo", "Lucia"];
+const players = beginningOrder.length;
 
-const getPlayersOrder = (players, turns) => {
-    // Do implementation here
+const getRemainder = (turns, players) => {
+    return turns % players;
+}
+//1, 5, 9, 13, 17
+
+const getPlayersOrder = (turns, players, beginningOrder) => {
+    // ["Ana", "Juan", "Pablo", "Lucia"] 0 turn
+    // ["Juan", "Pablo", "Lucia", "Ana"] 1 turn
+    // ["Pablo", "Lucia", "Ana", "Juan"] 2 turn
+    // ["Lucia", "Ana", "Juan", "Pablo"] 3 turn
+    // ["Ana", "Juan", "Pablo", "Lucia"] 4 turn
+
+    console.log("BeginningOrderFunction", beginningOrder);
+
+    if (turns = 0) {
+        return beginningOrder;
+    } else if (getRemainder(1, 4) == 1) {
+        const firstPlayer = beginningOrder.shift();
+        const newOrder = beginningOrder.push(firstPlayer);
+        console.log(newOrder);
+    }
 };
+
+getPlayersOrder(1, 4, beginningOrder);
