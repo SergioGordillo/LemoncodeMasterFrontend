dc8c7b05a3e432bae5a926fe4b7be89340866f3f
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-08-08 20:21:58 +0200

    exercise nearly done, just lacking of TS configuration

--- 02-JavaScript/05-Deliverables/04-ReadBook/webpack.config.js
+++ 02-JavaScript/05-Deliverables/04-ReadBook/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: ["./main.js"],
     module: {
@@ -8,5 +10,13 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', //Name of file in ./dist/
+            template: 'index.html', //Name of template in ./src
+            scriptLoading: "blocking", // I want bundle to have it downloaded to start processing it
+            hash: true
+        }),
+    ]
 }
