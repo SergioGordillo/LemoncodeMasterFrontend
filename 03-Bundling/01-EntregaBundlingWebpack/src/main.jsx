f6f7f32619ba872bbdce53897c058124ca0353ba
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-08-11 23:17:50 +0200

    already working with React but I need to improve a little bug related to img

--- 03-Bundling/01-EntregaBundlingWebpack/src/main.jsx
+++ 03-Bundling/01-EntregaBundlingWebpack/src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.scss";
 
-
 const root = createRoot(document.getElementById("root"));
 root.render(
     <div>
-        <img class="escudo" src="../assets/img/escudo-arsenal.jpg" alt="Escudo del Arsenal" />
+        {/* <img class="escudo" src="../assets/img/escudo-arsenal.jpg" alt="Escudo del Arsenal" /> */}
         <h1>Hello Gunners</h1>
         <h2>Welcome to your favourite gunner fan site</h2>
     </div>
