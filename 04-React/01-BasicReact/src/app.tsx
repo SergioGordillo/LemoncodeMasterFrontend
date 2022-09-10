1bba1f7423c1d7c501fbb4745b5ddcf588f5060f
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-09-11 00:32:49 +0200

    doing table with MUI, stuff to fix still left

--- 04-React/01-BasicReact/src/app.tsx
+++ 04-React/01-BasicReact/src/app.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
+import BasicTable from "./table";
 
 export const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                <Route path="/list" element={<ListPage />} />
-                <Route path="/detail/:id" element={<DetailPage />} />
+                {/* <Route path="/list" element={<ListPage />} />
+                <Route path="/detail/:id" element={<DetailPage />} /> */}
+                <Route path="/table" element={<BasicTable />} />
             </Routes>
         </Router>
     );
--- 04-React/01-BasicReact/src/login.tsx
+++ 04-React/01-BasicReact/src/login.tsx
@@ -10,7 +10,7 @@ export const LoginPage: React.FC = () => {
         e.preventDefault();
 
         if (username === "admin" && password === "test") {
-            navigate("/list");
+            navigate("/table");
         } else {
             alert("User / password not valid, psst... admin / test");
         }
