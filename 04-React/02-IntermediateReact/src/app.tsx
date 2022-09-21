87a2aca9e48d0f0a04301c5d818d1c03ba50d073
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-09-21 20:17:24 +0200

    doing the basics installations to start working, need to test if MUI has been properly installed

---
+++ 04-React/02-IntermediateReact/src/app.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+export const App = () => {
+    return (
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" element={<LoginPage />} />
+        //         <Route path="/list" element={<ListPage />} />
+        //         <Route path="/detail/:id" element={<DetailPage />} />
+        //         {/* <Route path="/table" element={<BasicTable />} /> */}
+        //     </Routes>
+        // </Router>
+        <h1>Hello World</h1>
+    );
+};
