39833281a8b3046190cdffb685be160dc1db6b40
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-09-22 18:02:17 +0200

    some basic improvements done

--- 04-React/01-BasicReact/src/app.tsx
+++ 04-React/01-BasicReact/src/app.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
-import BasicTable from "./table";
 
 export const App = () => {
     return (
@@ -12,7 +11,6 @@ export const App = () => {
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/list" element={<ListPage />} />
                 <Route path="/detail/:id" element={<DetailPage />} />
-                {/* <Route path="/table" element={<BasicTable />} /> */}
             </Routes>
         </Router>
     );
