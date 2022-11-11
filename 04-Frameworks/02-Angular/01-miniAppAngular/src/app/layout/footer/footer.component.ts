5a67c284a9fe1d7400a01fc6321ce0f63674338b
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-11-11 20:31:07 +0100

    ready to start point 5 of Angular work

--- 04-Frameworks/02-Angular/01-miniAppAngular/src/app/layout/footer/footer.component.ts
+++ 04-Frameworks/02-Angular/01-miniAppAngular/src/app/layout/footer/footer.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+
+  public time: Date = new Date();
+  public year: number = this.time.getFullYear();
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
