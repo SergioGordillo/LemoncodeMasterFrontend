73f1d9cc6d03b99817c729cc1fd1cb94c5298dd2
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-01-02 13:08:30 +0100

    first commit, setup done

---
+++ TFM/src/app.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { MyDownloadPage } from "./components/myDownloadPage";
+
+export const App = () => {
+  return <MyDownloadPage />;
+};
---
+++ TFM/src/components/myDownloadPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MyDownloadPage: React.FC = () => {
+    return (
+        <>
+            <h2>Hello World</h2>
+        </>
+    );
+};
