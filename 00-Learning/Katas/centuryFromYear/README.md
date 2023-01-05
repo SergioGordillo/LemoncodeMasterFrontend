ffd412e3eee0993b4a2b461108f4390aa769de5b
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-01-05 21:32:14 +0100

    working on algorythm

--- 00-Learning/Katas/centuryFromYear/README.md
+++ 00-Learning/Katas/centuryFromYear/README.md
@@ -1,4 +1,4 @@
-Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second from the year 101 and including the year 200 etc.
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second from the year 101 and including the year 200 etc. Input and output is int.
 
 function centuryFromYear(year){
 //YOUR CODE
--- 00-Learning/Katas/centuryFromYear/main.js
+++ 00-Learning/Katas/centuryFromYear/main.js
@@ -1,7 +1,17 @@
 function centuryFromYear(year){
-    let text = year.toString(); 
-    console.log(text); 
-    console.log(typeof(text));
+    if(year>=1 && year <=2005){
+        return "working";
+    }else{
+        return "You have to introduce a number between 1 and 2005";
+    }
 }
 
-centuryFromYear(1);
+console.log(centuryFromYear(3));
+console.log(centuryFromYear("dos"));
+
+
+// year= 1905, output= 20
+// year= 1700, output= 17.
+//1-100-> siglo I
+//101-200->siglo II
+//year = 1-year-2005
