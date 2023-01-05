cbedd62d4bb5a2cb872831fecf613b3281639737
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-01-05 21:48:34 +0100

    centuryfromyear done

--- 00-Learning/Katas/centuryFromYear/main.js
+++ 00-Learning/Katas/centuryFromYear/main.js
@@ -1,17 +1,6 @@
 function centuryFromYear(year){
-    if(year>=1 && year <=2005){
-        return "working";
-    }else{
-        return "You have to introduce a number between 1 and 2005";
-    }
+    return Math.ceil(year/100);
 }
 
-console.log(centuryFromYear(3));
-console.log(centuryFromYear("dos"));
-
-
-// year= 1905, output= 20
-// year= 1700, output= 17.
-//1-100-> siglo I
-//101-200->siglo II
-//year = 1-year-2005
+console.log(centuryFromYear(1900));
+console.log(centuryFromYear(1901));
