aeaab66a3c83c3f7b83ea99d1a8243b476e432b8
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-01-23 21:29:36 +0100

    task done

--- TFM/src/components/myDownloadPage.tsx
+++ TFM/src/components/myDownloadPage.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import { saveAs } from "file-saver";
+
 export const MyDownloadPage: React.FC = () => {
   const download = () => {
-    console.log("Hello World");
+    Packer.toBlob(doc).then((blob) => {
+      saveAs(blob, "example.docx");
+    });
   };
+
+  const doc = new Document({
+    sections: [
+      {
+        properties: {},
+        children: [
+          new Paragraph({
+            children: [new TextRun("Hello World")],
+          }),
+        ],
+      },
+    ],
+  });
 
   return (
     <>
