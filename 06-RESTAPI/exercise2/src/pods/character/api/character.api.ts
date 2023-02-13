d3ed51cb670ac047f07ae04f9cbb28ac14fb6489
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-02-13 20:45:31 +0100

    exercise 2 done

--- 06-RESTAPI/exercise2/src/pods/character/api/character.api.ts
+++ 06-RESTAPI/exercise2/src/pods/character/api/character.api.ts
@@ -9,5 +9,14 @@ export const getCharacter = async (id: number): Promise<Character> => {
 };
 
 export const saveCharacter = async (character: Character): Promise<boolean> => {
+  const id = character.id;
+  const url = `http://localhost:3000/results/${id}`;
+
+  if (character.id) {
+    let saved = await axios.put<any>(url, character);
+  } else {
+    let saved = await axios.post<any>(url, character);
+  }
+
   return true;
 };
