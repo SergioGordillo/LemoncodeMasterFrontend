983c01372d4862613c255a08c07d6577bf33ebd4
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-03-01 12:20:07 +0100

    I have to configure dynamic id for graphql character petition and thats all

--- 06-RESTAPI/exercise3/src/pods/character/api/character.api.ts
+++ 06-RESTAPI/exercise3/src/pods/character/api/character.api.ts
@@ -1,16 +1,45 @@
 import { Character } from './character.api-model';
-// import { Lookup } from 'common/models';
 import axios from 'axios';
+import request, { gql } from 'graphql-request';
+
+const url = 'https://rickandmortyapi.com/graphql';
 
 export const getCharacter = async (id: number): Promise<Character> => {
-  const url = `https://rickandmortyapi.com/api/character/${id}`;
-  const { data } = await axios.get<any>(url);
+  // const url = `https://rickandmortyapi.com/api/character/${id}`;
+  // const { data } = await axios.get<any>(url);
+  // return data;
+  const query = gql`
+    query ($id: ID!) {
+      character(id: $id) {
+        id
+        name
+        status
+        species
+        type
+        gender
+        origin {
+          name
+        }
+        location {
+          name
+        }
+        image
+        episode {
+          episode
+        }
+        created
+      }
+    }
+  `;
+
+  const variables = {
+    id: '3',
+  };
+
+  const data = await request(url, query, variables);
+  console.log('data', data);
   return data;
 };
-
-// export const getCities = async (): Promise<Lookup[]> => {
-//   return mockCities;
-// };
 
 export const saveCharacter = async (character: Character): Promise<boolean> => {
   return true;
